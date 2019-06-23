df6a6fb74379710f6ff1e3ea90ba5a75965b6482
application_for_network <legioneres151@gmail.com>
2019-06-23 16:55:03 +0300

    The bug with question input fild was resolved

--- app/src/main/java/com/example/admin/aplicationforcursaci/HelpActivity.java
+++ app/src/main/java/com/example/admin/aplicationforcursaci/HelpActivity.java
@@ -14,31 +14,34 @@ public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText edText = findViewById(R.id.editText10);
-                edText.setText("");
-                Toast toast = Toast.makeText(HelpActivity.this,"Ваш вопрос был успешно отправлен, мы ответим как только появится возможноть",Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP,0,200);
-                toast.show();
+                if (edText.getText().length() > 0) {
+                    edText.setText("");
+                    Toast toast = Toast.makeText(HelpActivity.this, "Ваш вопрос был успешно отправлен, мы ответим как только появится возможноть", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.TOP, 0, 200);
+                    toast.show();
+                }
             }
         });
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id == android.R.id.home){
+        if (id == android.R.id.home) {
             finish();
         }
         return super.onOptionsItemSelected(item);
